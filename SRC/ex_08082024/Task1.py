cb9b599936cae0470ee617e0435fe36a4fe0c051
girish4python <girish.stockimpact@gmail.com>
2024-08-19 23:14:01 +0530

    task 1

---
+++ SRC/ex_08082024/Task1.py
@@ -0,0 +1,20 @@
+#Task1
+#Home Can you create a Program I will give you number(userinput and print table)
+#f"{}" String format concept
+#User input - num int -> 10, 100, -1, 2, 3.14 -> input
+
+user_input = int(input("Enter a number : "))
+print(f"{user_input}x1={user_input*1}")
+print(f"{user_input}x2={user_input*2}")
+print(f"{user_input}x3={user_input*3}")
+print(f"{user_input}x3={user_input*4}")
+print(f"{user_input}x5={user_input*5}")
+print(f"{user_input}x6={user_input*6}")
+print(f"{user_input}x7={user_input*7}")
+print(f"{user_input}x8={user_input*8}")
+print(f"{user_input}x9={user_input*9}")
+print(f"{user_input}x10={user_input*10}")
+
+
+
+
