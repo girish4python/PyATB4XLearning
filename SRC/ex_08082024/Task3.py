1e68ba6ae730fae8b20ca06165312b40a5042a59
girish4python <girish.stockimpact@gmail.com>
2024-08-21 22:50:48 +0530

    Task 3

--- SRC/ex_08082024/Task3.py
+++ SRC/ex_08082024/Task3.py
@@ -5,3 +5,6 @@
 
 # 2. What does the ** operator do in Python, and how is it used?
 #  ** operator is used for exponentiation
+
+# 3. What does the ^ operator do in Python, and in what context is it commonly used?
+#  ^ is a XOR operator
