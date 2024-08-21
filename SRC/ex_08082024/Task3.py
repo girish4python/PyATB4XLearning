70e4de19da4af993f1a3b1b0801a9b3e3ed58893
girish4python <girish.stockimpact@gmail.com>
2024-08-21 22:28:17 +0530

    Task 3

--- SRC/ex_08082024/Task3.py
+++ SRC/ex_08082024/Task3.py
@@ -1,1 +1,7 @@
-print("hello")
+# 1 . Explain the difference between the = operator and the == operator in Python.
+
+#  = is used to assign a value to a variable while == is an assignment operator
+
+
+# 2. What does the ** operator do in Python, and how is it used?
+#  ** operator is used for exponentiation
