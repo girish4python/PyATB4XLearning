6b02861f9fa23e1e8e65414b807610ca0fd81254
girish4python <girish.stockimpact@gmail.com>
2024-08-21 22:21:40 +0530

    Task 2

--- SRC/ex_08082024/Task2.py
+++ SRC/ex_08082024/Task2.py
@@ -1,3 +1,11 @@
+# TASK2
+# Create a program , take 2 inputs from the user num1, num2 and give them
+# max
+# pow num1 to num2
+# sub, mul, sum, div.
+# Format your out with f{""}
+
+
 a = int(input("Enter Number 1 :  "))
 b = int(input("Enter Number 2 : "))
 
@@ -7,7 +15,5 @@ print(f"Maximum number is {Max_num}")
 Power =pow(a,b)
 print(f"Power of number is {Power}")
 
-mul=print("Multiplication is ", a*b)
-sum=print("Additon is", a+b)
-minus=print("Substractio is", a-b)
-divide=print("Division is", a/b)
+sub = a - b
+print(f"sub is {sub}")
