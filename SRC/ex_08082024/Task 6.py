7d7b9205534f96f36406fe4a72ec7b47578025b6
girish4python <girish.stockimpact@gmail.com>
2024-08-21 23:24:44 +0530

    Task 6

---
+++ SRC/ex_08082024/Task 6.py
@@ -0,0 +1,8 @@
+# Task #6
+# Develop a Python script that calculates the square and cube of a given number.
+
+num = int(input("enter a number : "))
+sqr = num**2
+print(f"The square of the number is {sqr}")
+cube = num**3
+print(f"The square of the number is {cube}")
