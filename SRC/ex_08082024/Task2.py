3760b018529bdb9e4768376da1da212bea29332f
girish4python <girish.stockimpact@gmail.com>
2024-08-21 21:57:57 +0530

    task 2

---
+++ SRC/ex_08082024/Task2.py
@@ -0,0 +1,13 @@
+a = int(input("Enter Number 1 :  "))
+b = int(input("Enter Number 2 : "))
+
+Max_num=max(a,b)
+print(f"Maximum number is {Max_num}")
+
+Power =pow(a,b)
+print(f"Power of number is {Power}")
+
+mul=print("Multiplication is ", a*b)
+sum=print("Additon is", a+b)
+minus=print("Substractio is", a-b)
+divide=print("Division is", a/b)
