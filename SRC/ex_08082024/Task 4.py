47ca13a49936e0ae33ba7a26ccef17f4a9d13303
girish4python <girish.stockimpact@gmail.com>
2024-08-21 22:55:56 +0530

    Task 4

---
+++ SRC/ex_08082024/Task 4.py
@@ -0,0 +1,11 @@
+# Write a python program to calculate the area of the circle
+#
+# radius--> take input from the user
+# value of pi= 3.14
+# output--> float, name-->area
+
+
+rad = float(input("enter the value of radius : "))
+pi = 3.14
+area = 3.14 * (rad**2)
+print(f"The area of the circle is {area}")
